581abde23e53f2cf78146fa386cd1b1a1f87266c
Alfred Essa <malpaso@alfredcodes.com>
2025-12-19 18:08:38 -0500

    fix: use neon-http drizzle adapter to satisfy build

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { sql } from "drizzle-orm";
 
